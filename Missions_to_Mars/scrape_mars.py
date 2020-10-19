3e4755a10fd2e8b4f4a7f96894ae8bce1937eb5e
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-10-19 14:55:11 -0500

    Create scrape_mars.py
    Update

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,103 @@
+#import dependencies
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+from splinter import Browser
+
+#define your browser 
+def init_browser():
+     executable_path = {'executable_path': 'chromedriver.exe'}
+     browser = Browser('chrome', **executable_path, headless=False)
+     return browser
+
+
+#Scrape Mars News Data
+mars_data = {}
+def scrape_news():
+    browser = init_browser()
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    #HTML object
+    html = browser.html
+    soup = bs(html,'html.parser')
+
+    #look for snippet
+    article_title = soup.find('div',class_='list_text').find('div', class_='content_title').find('a').text
+    article_paragraph = soup.find('div',class_='list_text').find('div', class_='article_teaser_body').text
+    article = [article_title,article_paragraph]
+    
+    #print it
+    return article
+    
+
+#Scrape Mars Images 
+def scrape_image():
+    browser = init_browser()
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+    
+    #HTML object
+    html = browser.html
+    soup = bs(html,'html.parser')
+    
+    #Image URL Link
+    base_url = 'https://www.jpl.nasa.gov/'
+    image_path_url = soup.find('div', class_='carousel_container').find('article')['style'].replace(');','')[23:-1]
+    featured_img_url = base_url + image_path_url
+    
+    #print it
+    return featured_img_url
+
+#Scrape Hemisphere Data    
+def scrape_hemi():
+    browser = init_browser()
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    
+     #HTML object
+    browser.visit(url)
+    html = browser.html
+
+    #shorten URL
+    shorter_url = 'https://astrogeology.usgs.gov'
+    
+    #pull info
+    browser.visit(url_4)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    main_url = soup.find_all('div', class_='item')
+    titles=[]
+    hemisphere_img_urls=[]
+
+    #loop through data to create URL
+    for x in main_url:
+    title = x.find('h3').text
+    url = x.find('a')['href']
+    hem_img_url = shorter_url + url
+    
+    #print(hem_img_url)
+    browser.visit(hem_img_url)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    hem_imgage_origin = soup.find('div',class_='downloads')
+    hem_imgage_url= hem_imgage_origin.find('a')['href']
+    
+    print(hemisphere_img_url)
+    img_data = dict({'title':title, 'img_url':hemisphere_img_url})
+    hemisphere_img_urls.append(img_data)
+    
+
+def scrape():
+    article1 = scrape_new()
+    title = article1[0]
+    paragraph = article1[1]
+    featured_image = scrape_featured()
+    facts = scrape_facts()
+    hemispheres = scrape_hemi()
+
+    compressed_scrape = {'news_title':title,
+                        'news_paragraph':paragraph,
+                        'featured_img':featured_image,
+                        'facts':facts,
+                        'hemispheres':hemispheres}
+    return compressed_scrape
