736a44c308fcd97dbd15d8084d2492064502be79
crissymlee <66131888+crissymlee@users.noreply.github.com>
2020-10-19 18:58:12 -0500

    Update working server

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -3,23 +3,22 @@ from flask_pymongo import PyMongo
 import scrape_mars
 
 app = Flask(__name__)
-conn = 'mongodb://localhost:27017'
-mongo = pymongo.MongoClient(conn)
-db = mongo.mars_db
-collection = db.mars_info
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars'
+db = PyMongo(app)
+print(db)
 
 
 @app.route("/")
 def index():
-   mars = collection.find_one()
+   mars = db.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
-    mars = collection
+    mars = db
     mars_data = scrape_mars.scrape()
-    mars.update({}, mars_data, upsert=True)
+    db.db.mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,103 +3,114 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 from splinter import Browser
+import time
 
-#define your browser 
+# define your browser
+
+
 def init_browser():
-     executable_path = {'executable_path': 'chromedriver.exe'}
-     browser = Browser('chrome', **executable_path, headless=False)
-     return browser
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    return browser
 
 
-#Scrape Mars News Data
+# Scrape Mars News Data
 mars_data = {}
+
+
 def scrape_news():
     browser = init_browser()
     url_1 = 'https://mars.nasa.gov/news'
     browser.visit(url_1)
-
-    #HTML object
+    time.sleep(5)
+    # HTML object
     html = browser.html
-    soup = bs(html,'html.parser')
+    soup = bs(html, 'html.parser')
 
-    #look for snippet
-    article_title = soup.find('div',class_='list_text').find('div', class_='content_title').find('a').text
-    article_paragraph = soup.find('div',class_='list_text').find('div', class_='article_teaser_body').text
-    
-    #print it
-    return article_title
-    
+    # look for snippet
+    article_title = soup.find('div', class_='list_text').find(
+        'div', class_='content_title').find('a').text
+    article_paragraph = soup.find('div', class_='list_text').find(
+        'div', class_='article_teaser_body').text
 
-#Scrape Mars Images 
+    # print it
+    return article_title, article_paragraph
+
+
+# Scrape Mars Images
 def scrape_image():
     browser = init_browser()
     url_2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(url_2)
-    
-    #HTML object
-    html = browser.html
-    soup = bs(html,'html.parser')
-    
-    #Image URL Link
-    main_url ='https://www.jpl.nasa.gov'
-    image_url = soup.find('div', class_='carousel_container').find('article')['style'].replace(');','')[1:-1]
 
-    #Create image url link
+    # HTML object
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    # Image URL Link
+    main_url = 'https://www.jpl.nasa.gov'
+    image_url = soup.find('div', class_='carousel_container').find(
+        'article')['style'].replace(');', '')[1:-1]
+
+    # Create image url link
     image_url = main_url + image_url
     print(image_url)
-    
-    #print it
-    return featured_img_url
 
-#Scrape Hemisphere Data    
+    # print it
+    return image_url
+
+# Scrape Hemisphere Data
+
+
 def scrape_hemi():
     browser = init_browser()
-    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    
-     #HTML object
-    browser.visit(url)
+    url_4 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+    # HTML object
+    browser.visit(url_4)
     html = browser.html
 
-    #shorten URL
+    # shorten URL
     shorter_url = 'https://astrogeology.usgs.gov'
-    
-    #pull info
+
+    # pull info
     browser.visit(url_4)
     html = browser.html
     soup = bs(html, 'html.parser')
     main_url = soup.find_all('div', class_='item')
-    titles=[]
-    hemisphere_img_urls=[]
+    titles = []
+    hemisphere_img_urls = []
 
-    #loop through data to create URL
+    # loop through data to create URL
     for x in main_url:
-    title = x.find('h3').text
-    url = x.find('a')['href']
-    hem_img_url = shorter_url + url
-    
-    #print(hem_img_url)
-    browser.visit(hem_img_url)
-    html = browser.html
-    soup = bs(html, 'html.parser')
-    hem_imgage_origin = soup.find('div',class_='downloads')
-    hem_imgage_url= hem_imgage_origin.find('a')['href']
-    
-    print(hemisphere_img_url)
-    img_data = dict({'title':title, 'img_url':hemisphere_img_url})
-    hemisphere_img_urls.append(img_data)
-    
+        title = x.find('h3').text
+        url = x.find('a')['href']
+        hem_img_url = shorter_url + url
+
+        # print(hem_img_url)
+        browser.visit(hem_img_url)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+        hem_imgage_origin = soup.find('div', class_='downloads')
+        hemisphere_img_url = hem_imgage_origin.find('a')['href']
+
+        print(hemisphere_img_url)
+        img_data = dict({'title': title, 'img_url': hemisphere_img_url})
+        hemisphere_img_urls.append(img_data)
+        return hemisphere_img_urls
+
 
 def scrape():
-    article1 = scrape_new()
+    article1 = scrape_news()
     title = article1[0]
     paragraph = article1[1]
-    featured_image = scrape_featured()
-    facts = scrape_facts()
+    featured_image = scrape_image()
+    facts = "<table></table>"
     hemispheres = scrape_hemi()
 
-    compressed_scrape = {'news_title':title,
-                        'news_paragraph':paragraph,
-                        'featured_img':featured_image,
-                        'facts':facts,
-                        'hemispheres':hemispheres}
+    compressed_scrape = {'news_title': title,
+                         'news_paragraph': paragraph,
+                         'featured_img': featured_image,
+                         'facts': facts,
+                         'hemispheres': hemispheres}
     return compressed_scrape
